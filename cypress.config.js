e7b74e712095835f17bd3925f0a6db7016673a33
lekuro <lekurot@gmail.com>
2026-05-08 19:55:27 +0300

    Add retries for better CI stability

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   projectId: "42miv5",
   viewportWidth: 1920,
   viewportHeight: 1080,
+  retries: {
+    runMode: 2,
+    openMode: 0
+  },
   e2e: {
     baseUrl: 'https://telnyx.com',
     setupNodeEvents(on, config) {
--- cypress.custom.config.js
+++ cypress.custom.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   projectId: "42miv5",
   viewportWidth: 1920,
   viewportHeight: 1080,
+  retries: {
+    runMode: 2,
+    openMode: 0
+  },
   defaultCommandTimeout: 10000,
   e2e: {
     baseUrl: 'https://telnyx.com',
